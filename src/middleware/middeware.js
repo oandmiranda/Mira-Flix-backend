b414838edca0f410c8c85c8f33fb8b5195dc1b07
Anderson Miranda <and.miranda1818@gmail.com>
2024-10-22 18:24:33 -0300

    add origin urls

--- src/middleware/middeware.js
+++ src/middleware/middeware.js
@@ -2,11 +2,15 @@ import express from 'express';
 import cors from 'cors';
 
 export const corsOpitions = {
-    origin: ['http://localhost:3000', 'https://mira-flix-frontend.vercel.app'],
+    origin: [
+        'http://localhost:3000', 
+        'https://mira-flix-frontend.vercel.app', 
+        'https://mira-flix-frontend-git-dev-oandmirandas-projects.vercel.app'  // Adicione essa URL
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     optionsSuccessStatus: 200
-}
+};
 
 // Middleware CORS
 export const corsMiddleware = cors(corsOpitions);
